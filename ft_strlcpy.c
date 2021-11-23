b709a01a7d4103c3ad28ee615675471b9b8a0257
MiiSantos <miriaandressa1@gmail.com>
2021-11-23 19:16:44 -0300

    protected

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mandress <mandress@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 21:49:54 by mandress          #+#    #+#             */
-/*   Updated: 2021/11/23 19:03:21 by mandress         ###   ########.fr       */
+/*   Updated: 2021/11/23 19:07:04 by mandress         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 	size_t	i;
 
 	i = 0;
-	if (!src || !dest)
-		return (0);
 	if (size > 0)
 	{
 		while (src[i] != '\0' && i < (size - 1))
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mandress <mandress@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 18:00:06 by mandress          #+#    #+#             */
-/*   Updated: 2021/11/23 19:02:12 by mandress         ###   ########.fr       */
+/*   Updated: 2021/11/23 19:13:23 by mandress         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 {
 	char	*sub;
 	size_t	size;
+	size_t	i;
 
+	i = 0;
 	size = ft_strlen(s);
 	if (!s)
 		return (0);
@@ -27,6 +29,12 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	sub = (char *)malloc((sizeof(char) * len) + 1);
 	if (!sub)
 		return (0);
-	ft_strlcpy(sub, &s[start], len + 1);
+	while (len > 0)
+	{
+		sub[i] = s[i + start];
+		i++;
+		len--;
+	}
+	sub[i] = '\0';
 	return (sub);
 }
